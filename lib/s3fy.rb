c48c13499ad4a7a6ac1ac9bd46cb3e3ef3bacb3c
Breno Moura <basmoura@gmail.com>
2015-02-12 16:19:47 -0300

    initial commit

---
+++ lib/s3fy.rb
@@ -0,0 +1,5 @@
+require "s3fy/version"
+
+module S3fy
+  # Your code goes here...
+end
---
+++ spec/s3fy_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe S3fy do
+  it 'has a version number' do
+    expect(S3fy::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
